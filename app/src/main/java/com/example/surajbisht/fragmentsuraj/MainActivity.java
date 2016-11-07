439f6ac8602c0f7388c7b2c2aae1c3b0d245fbc9
Suraj <bishtsuraj95@gmail.com>
2016-11-07 17:47:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/surajbisht/fragmentsuraj/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.surajbisht.fragmentsuraj;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et2= (EditText) findViewById(R.id.et2);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        et2.setCompoundDrawablesWithIntrinsicBounds(R.drawable.);
+    }
+public void login()
+{
+
+}
+}
+
